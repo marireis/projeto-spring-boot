bbad82ae6a6b9d3a3f424da3b1d1f153bed4bc3e
Marina Reis <marinareis.mr@gmail.com>
2023-04-24 11:16:15 -0300

    inciiando projeto

---
+++ src/main/java/br/com/cod3r/springboot/controllers/PrimeiroController.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.springboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+
+	@GetMapping(path = {"/ola", "saudacao"})//protocolo de requisição do tipo GET
+	public String ola() {
+		return "Olá Spring Boot!";
+	}
+}
