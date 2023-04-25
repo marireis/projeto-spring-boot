a11a3ccf0aa1d2bb215d345e5c6f12ce9c9cfb08
Marina Reis <marinareis.mr@gmail.com>
2023-04-25 13:23:17 -0300

    inserindo metodo http get

---
+++ src/main/java/br/com/cod3r/springboot/controllers/CalculadoraController.java
@@ -0,0 +1,29 @@
+package br.com.cod3r.springboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/calculadora")
+public class CalculadoraController {
+
+	//path variable insere na url http://localhost:8083/calculadora/somar/2/3
+	@GetMapping("/somar/{a}/{b}")
+	public int somar(@PathVariable int a, @PathVariable int b) {
+		
+		return a + b;
+	}
+	
+
+	//request param insere direto o valor de a e b http://localhost:8083/calculadora/subtrair?b=100&a=30
+	@GetMapping("/subtrair")
+	public int subtrair(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b) {
+		
+		return a - b;
+	}
+	
+}
---
+++ src/main/java/br/com/cod3r/springboot/controllers/MetodosHttpController.java
@@ -0,0 +1,35 @@
+package br.com.cod3r.springboot.controllers;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/metodos")
+public class MetodosHttpController {
+
+	@GetMapping
+	public String get() {
+		return "Requisição GET";
+	}
+	@PostMapping
+	public String post() {
+		return "Requisição POST";
+	}
+	@PutMapping
+	public String put() {
+		return "Requisição PUT";
+	}
+	@PatchMapping
+	public String path() {
+		return "Requisição PATH";
+	}
+	@DeleteMapping
+	public String delete() {
+		return "Requisição DELETE";
+	}
+}
