b852904437d23d01ca65ba4df9c90f2ea70f3ea3
Marina Reis <marinareis.mr@gmail.com>
2023-04-28 13:44:38 -0300

    implementando findAll e findById

--- src/main/java/br/com/cod3r/springboot/controller/ProdutoController.java
+++ src/main/java/br/com/cod3r/springboot/controller/ProdutoController.java
@@ -1,10 +1,14 @@
 package br.com.cod3r.springboot.controller;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +23,20 @@ public class ProdutoController {
 	private ProdutoRepository produtoRepository;
 
 	@PostMapping
-	public @ResponseBody Produto novoProduto(@RequestBody Produto produto) {
+	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 		
 		produtoRepository.save(produto);
 		return produto;
+	}
+	
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+	
+		return produtoRepository.findAll();//retorna um interável de produto
+	}
+	
+	@GetMapping(path="/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
 	}
 }
