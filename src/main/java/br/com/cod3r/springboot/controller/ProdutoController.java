578bb618c33139d3baa6ec52c22dc42efb06bdcf
Marina Reis <marinareis.mr@gmail.com>
2023-04-28 14:17:51 -0300

    crud

--- src/main/java/br/com/cod3r/springboot/controller/ProdutoController.java
+++ src/main/java/br/com/cod3r/springboot/controller/ProdutoController.java
@@ -5,9 +5,13 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +42,36 @@ public class ProdutoController {
 	@GetMapping(path="/{id}")
 	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
 		return produtoRepository.findById(id);
+	}
+	
+	@PutMapping
+	public Produto alterarProduto(@Valid Produto produto) {
+		produtoRepository.save(produto);
+		return produto;
+	}
+	
+	//juntando o método PUT E POST
+	/*
+	 * @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	 * public @ResponseBody Produto salvarProduto(@Valid Produto produto ){
+	 * 	produtoRepository.save(produto);
+	 * 	return produto;
+	 *  */
+	
+	@DeleteMapping("/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+	}
+	
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
+	public Iterable<Produto> obterProdutoPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdePagina){
+		Pageable page = PageRequest.of(numeroPagina,qtdePagina);
+		return produtoRepository.findAll(page);
+		
+	}
+	
+	@GetMapping(path="/nome/{parteNome}")
+	public Iterable<Produto> obterProdutoPorNome(@PathVariable String parteNome) {
+		return produtoRepository.findByNomeContaining(parteNome);
 	}
 }
--- src/main/java/br/com/cod3r/springboot/repository/ProdutoRepository.java
+++ src/main/java/br/com/cod3r/springboot/repository/ProdutoRepository.java
@@ -1,12 +1,13 @@
 package br.com.cod3r.springboot.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.cod3r.springboot.model.entity.Produto;
 
 @Repository
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
-		
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
+
+	public Iterable<Produto> findByNomeContaining(String parteNome);
 }
 
